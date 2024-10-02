b36eb98ce8d06f313b1c25c14bb2f26fb857c825
Polly Bel <polialekseevna80@gmail.com>
2024-10-02 20:29:25 +0300

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 print("Дано число n в восьмеричной СС состоящие из 5 чисел. Выведите на экран значение этого числа в десятичной системе счисления")
-print("введите число в восмеричной сс")
-n=input()
-list_n=[int(x) for x in n]
-reversed_list=list(reversed(list_n))
-sum=0
-for int , val in enumerate (reversed_list):
-    sum+=  val*(8**int)
-    print(sum)
+print("Введите пятизначное число в восмеричной сс")
+n = int(input())
+eight_last = n % 10
+n //= 10
+ten_last = eight_last * (8**0)
+eight_prev = n % 10
+n //= 10
+ten_prev = eight_prev * (8**1)
+eight_third = n % 10
+n //= 10
+ten_third = eight_third * (8**2)
+eight_second = n % 10
+n //= 10
+ten_second = eight_second * (8**3)
+eight_first = n % 10
+n //= 10
+ten_first = eight_first * (8**4)
+sum = ten_last + ten_prev + ten_third + ten_second + ten_first
+print("Число в десятичной сс ", sum)
